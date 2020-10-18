d5d81dbb73dffe52d0789ba1a7a3788a3f00518e
marcosroda <marcos.roda@gmail.com>
2020-10-18 18:27:31 -0300

    Retirada do build com gradle Inclusão de modelo web Inclusão de modelo api

--- src/test/java/CucumberTestSuite.java
+++ src/test/java/CucumberTestSuite.java
@@ -1,5 +1,3 @@
-package starter;
-
 import io.cucumber.junit.CucumberOptions;
 import net.serenitybdd.cucumber.CucumberWithSerenity;
 import org.junit.runner.RunWith;
@@ -7,6 +5,7 @@ import org.junit.runner.RunWith;
 @RunWith(CucumberWithSerenity.class)
 @CucumberOptions(
         plugin = {"pretty"},
-        features = "src/test/resources/features"
+        features = "src/test/resources/features",
+        glue = {"stepDefinition"}
 )
 public class CucumberTestSuite {}
